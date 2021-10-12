673b71415c7f8eeac019cddc0b51fd3df27487c9
InfiniteAugust <44834947+InfiniteAugust@users.noreply.github.com>
2021-10-12 23:41:57 +0100

    Add files via upload

---
+++ app/generated/source/buildConfig/debug/cn/edu/nottingham/zy21724/mdpcw01/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package cn.edu.nottingham.zy21724.mdpcw01;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "cn.edu.nottingham.zy21724.mdpcw01";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
